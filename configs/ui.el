c599bc9b991b6e3a957e9114c745174ee100a9bd
koribot <w.cawasa@gmail.com>
2026-02-12 17:09:53 +0800

    add early-init

--- configs/ui.el
+++ configs/ui.el
@@ -1,11 +1,10 @@
 ;;; UI Appearance Settings
-(setq inhibit-startup-screen t)
-(setq frame-title-format "koribot24 - %b")
+
 ;; Don't show DOS line endings as ^M or $
 (setq inhibit-eol-conversion nil)
 (setq-default buffer-file-coding-system 'utf-8-unix)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
+
+;; Line numbers
 (global-display-line-numbers-mode 1)
 
 ;; Safe hideshowvis hook - only runs if package is available
@@ -14,27 +13,21 @@
             (when (fboundp 'hideshowvis-enable)
               (hideshowvis-enable))))
 
-;; Frame settings
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
-(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
-;; Disable bell
-(setq ring-bell-function 'ignore)
 ;; Dired settings
 (add-hook 'dired-mode-hook
           (lambda ()
            (local-set-key [mouse-1] 'dired-find-file)))
+
 ;; Save and restore sessions automatically
 (desktop-save-mode 1)
 (setq desktop-restore-frames nil)  ; Don't restore frame configuration
-(setq desktop-lazy-verbose nil)  ; Less messages during lazy loading
+(setq desktop-lazy-verbose nil)    ; Less messages during lazy loading
 (setq desktop-lazy-idle-delay 1)   ; Wait 1 second before lazy loading
-(setq desktop-restore-eager 1)  ; Load first 3 buffers immediately, rest lazy
+(setq desktop-restore-eager 1)     ; Load first buffer immediately, rest lazy
+(setq desktop-auto-save-timeout 300) ; Auto-save every 5 minutes
+
+;; Indentation
 (electric-indent-mode -1)
 (global-set-key (kbd "RET") 'newline-and-indent)
-;; Add this line at the top of your init file for line spacing
-(setq-default line-spacing 4)
-(set-face-attribute 'default nil :font "Lucida Console-11")
-; Auto-save every 5 minutes
-(setq desktop-auto-save-timeout 300) 
+
 (provide 'ui)
--- init.el
+++ init.el
@@ -1,12 +1,30 @@
-;;; -*- lexical-binding: t -*-
+;;; init.el --- Main initialization file -*- lexical-binding: t -*-
 
-;;; Package setup
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Restore garbage collection and file handlers after startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 20 1000 1000)) ; 20mb
+            (setq file-name-handler-alist default-file-name-handler-alist)))
+
+;; Initialize package system (since we disabled auto-init in early-init.el)
 (package-initialize)
 
-;;Add ~/.emacs.d/ to load path so Emacs can find your files
+;; Install critical dependencies first
+(defun ensure-package-installed (package)
+  "Install PACKAGE if not already installed."
+  (unless (package-installed-p package)
+    (unless package-archive-contents
+      (package-refresh-contents))
+    (package-install package)))
+
+;; Install core dependencies
+(ensure-package-installed 'dash)
+(ensure-package-installed 's)
+(ensure-package-installed 'popup)
+
+;; Add ~/.emacs.d/ to load path so Emacs can find your files
 (add-to-list 'load-path "~/.emacs.d/configs/")
+
 ;; Load configuration modules
 (require 'ui)
 (require 'completion)
@@ -15,28 +33,9 @@
 (require 'keybindings)
 (require 'dirs)
 
-;; Add this line at the top of your init file for line spacing
-(setq-default line-spacing 4)
-(set-face-attribute 'default nil :font "Lucida Console-11")
-
-
-
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(cursor ((t (:background "gainsboro"))))
-;;  '(dired-directory ((t (:foreground "gold3"))))
-;;  '(font-lock-function-name-face ((t (:foreground "#fce94f" :strike-through nil)))))
-
-
+;; Custom faces and variables
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#c8c8c0"))))
+ '(default ((t (:foreground "#c8c8c0" :font "Lucida Console-11"))))  ;; <-- ADD :font HERE
  '(cursor ((t (:background "#e8c080"))))
  '(dired-directory ((t (:foreground "#e0b070" :weight normal))))
  '(font-lock-builtin-face ((t (:foreground "#c8c8c0"))))
@@ -52,16 +51,11 @@
  '(lsp-face-highlight-textual ((t (:background "#2a2420"))))
  '(lsp-face-highlight-write ((t (:background "#342d26")))))
 
-
-
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tango-dark))
  '(global-whitespace-mode nil)
  '(package-selected-packages
-   '(counsel hideshowvis ivy marginalia orderless swiper-helm
-	     typescript-mode vertico))
+   '(counsel hideshowvis ivy marginalia orderless
+     swiper-helm typescript-mode vertico dumb-jump))
  '(tool-bar-mode nil))
+
