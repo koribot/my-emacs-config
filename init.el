70ece5543f6490bd67ab37f5f1159e4eb50ca7c8
Walid Cawasa <w.cawasa@gmail.com>
2026-03-13 00:16:15 +0800

    add font-guard on OS

--- init.el
+++ init.el
@@ -1,32 +1,24 @@
 ;;; init.el --- Main initialization file -*- lexical-binding: t -*-
-
 ;; Restore garbage collection and file handlers after startup
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq gc-cons-threshold (* 20 1000 1000)) ; 20mb
             (setq file-name-handler-alist default-file-name-handler-alist)))
-
 ;; Stop creating backup files (files ending with ~)
 ;;(setq make-backup-files nil)
-
 ;; Stop creating auto-save files (files starting and ending with #)
 ;;(setq auto-save-default nil)
-
 ;; Auto-create backup and auto-save folders if they don't exist
 (make-directory "~/.emacs.d/backups" t)
 (make-directory "~/.emacs.d/auto-saves" t)
-
 ;; Send all backup files to one folder
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 ;; Send all auto-save files to one folder
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-saves/" t)))
 ;; Still disable lock files
 (setq create-lockfiles nil)
-
-
 ;; Initialize package system (since we disabled auto-init in early-init.el)
 (package-initialize)
-
 ;; Install critical dependencies first
 (defun ensure-package-installed (package)
   "Install PACKAGE if not already installed."
@@ -34,15 +26,12 @@
     (unless package-archive-contents
       (package-refresh-contents))
     (package-install package)))
-
 ;; Install core dependencies
 (ensure-package-installed 'dash)
 (ensure-package-installed 's)
 (ensure-package-installed 'popup)
-
 ;; Add ~/.emacs.d/ to load path so Emacs can find your files
 (add-to-list 'load-path "~/.emacs.d/configs/")
-
 ;; Load configuration modules
 (require 'ui)
 (require 'completion)
@@ -51,13 +40,22 @@
 (require 'keybindings)
 (require 'dirs)
 
+;; Set font based on OS
+(cond
+  ((eq system-type 'windows-nt)
+   (when (find-font (font-spec :family "Lucida Console"))
+     (set-face-attribute 'default nil :family "Lucida Console" :height 110)))
+  ((eq system-type 'gnu/linux)
+   (when (find-font (font-spec :family "DejaVu Sans Mono"))
+     (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 110))))
+
 ;; Custom faces and variables
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#c8c8c0" :font "Lucida Console-11"))))
+ '(default ((t (:foreground "#c8c8c0"))))
  '(cursor ((t (:background "#e8c080"))))
  '(dired-directory ((t (:foreground "#e0b070" :weight normal))))
  '(font-lock-builtin-face ((t (:foreground "#c8c8c0"))))
@@ -72,7 +70,6 @@
  '(lsp-face-highlight-read ((t (:background "#2a2420"))))
  '(lsp-face-highlight-textual ((t (:background "#2a2420"))))
  '(lsp-face-highlight-write ((t (:background "#342d26")))))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -81,7 +78,6 @@
  '(custom-enabled-themes '(tango-dark))
  '(global-whitespace-mode nil)
  '(package-selected-packages
-   '(counsel dumb-jump go-mode hideshowvis ivy marginalia orderless
-	     swiper-helm typescript-mode vertico))
+   '(## counsel dumb-jump go-mode hideshowvis ivy marginalia orderless
+	swiper-helm typescript-mode vertico))
  '(tool-bar-mode nil))
-
